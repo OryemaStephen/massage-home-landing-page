07fa70fa82384ffcbc98a1fb1d6bc4aa45c84e19
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-01-08 12:53:26 +0300

    Added navbar
    Added navbar and made the page more responsive on different screen sizes.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Access all span elements with class="bullet"
 let list = document.querySelectorAll("span.bullet");
 
+// Add a number to each span element
 for(let i=0;i<=list.length; i++){
     if(i<10){
         list[i].innerHTML= "0"+(i+1);
